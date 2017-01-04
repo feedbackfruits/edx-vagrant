368737a5f51576ddcc4ae7d2ae23180d7ea4a4f8
Tiago Martins Araújo <tiago.martins.araujo@gmail.com>
2017-01-04 10:02:41 -0200

    including dummy box, setting instalation scripts to use eucalyptus release

--- Vagrantfile
+++ Vagrantfile
@@ -4,6 +4,8 @@ Vagrant.configure(2) do |config|
 
   config.vm.provider :digital_ocean do |provider, override|
     override.ssh.private_key_path = '~/.ssh/id_rsa'
+    override.vm.box = 'digital_ocean'
+    override.vm.box_url = 'https://github.com/devopsgroup-io/vagrant-digitalocean/raw/master/box/digital_ocean.box'
     provider.token = ENV['DIGITAL_OCEAN_TOKEN']
     provider.image = 'ubuntu-12-04-x64'
     provider.region = 'ams2'
--- provision.sh
+++ provision.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-echo "Running operating system updates..."
+
+echo "Running operating system updates"
 apt-get update -y
 apt-get upgrade -y
-echo "Installing required packages..."
-wget https://raw.githubusercontent.com/edx/configuration/master/util/install/ansible-bootstrap.sh -O - | sudo bash
-wget https://raw.githubusercontent.com/edx/configuration/master/util/install/sandbox.sh -O - | bash
+
+echo "Installing ansible-bootstrap.sh"
+wget https://raw.githubusercontent.com/edx/configuration/open-release/eucalyptus.master/util/install/ansible-bootstrap.sh -O - | sudo bash
+
+echo "Installing sandbox.sh"
+wget https://raw.githubusercontent.com/edx/configuration/open-release/eucalyptus.master/util/install/sandbox.sh -O - | bash
